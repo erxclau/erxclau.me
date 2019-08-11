cea9f0bcea8b030fcbae65957788def3b13b9231
ericlau00 <sharkguy418@gmail.com>
2019-08-11 17:23:32 -0400

    slide nav and projects

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function openNav() {
+    $("#navbar").css("width", "400px");
+    $(".nav-content").show();
+    $("#nav-close").css("visibility", "visible");
+    $("#nav-open").css("visibility", "hidden");
+    $(".main").css("margin-left", "425px");
+}
+function closeNav() {
+    $("#navbar").css("width", "75px");
+    $(".nav-content").hide();
+    $("#nav-close").css("visibility", "hidden");
+    $("#nav-open").css("visibility", "visible");
+    $(".main").css("margin-left", "80px");
+}
