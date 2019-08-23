f1f64bfa9b922faa6f1c837b7004c5696fec991e
ericlau00 <sharkguy418@gmail.com>
2019-08-23 04:13:11 -0400

    charting movies and reorganize js and collapse

--- js/app.js
+++ js/app.js
@@ -1,15 +1,79 @@
-function openNav() {
-    $("#navbar").css("width", "400px");
-    $(".nav-content").show();
-    $("#nav-close").css("visibility", "visible");
-    $("#nav-open").css("visibility", "hidden");
-    $(".main").css("margin-left", "425px");
-}
+App = {
 
-function closeNav() {
-    $("#navbar").css("width", "75px");
-    $(".nav-content").hide();
-    $("#nav-close").css("visibility", "hidden");
-    $("#nav-open").css("visibility", "visible");
-    $(".main").css("margin-left", "80px");
-}
+    init: function () {
+        App.listen();
+        App.getData();
+    },
+
+    listen: function () {
+        var collapsibles = $(".collapsible");
+        for (let i = 0; i < collapsibles.length; i++) {
+            collapsibles[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
+        }
+    },
+
+    getData: function () {
+        $.getJSON('./json/movies.json', function (data) {
+            App.createTable(data);
+        });
+    },
+
+    categorize: function (category, key) {
+        if (key in category) {
+            category[key]++;
+        } else {
+            category[key] = 1;
+        }
+    },
+
+    createTable: function (data) {
+        var time = new Object;
+        $.each(data, function (key, object) {
+            App.categorize(time, object.year + " " + object.month);
+        })
+        var context = $("#movies");
+        var chart = new Chart(context, {
+            type: 'bar',
+            data: {
+                labels: Object.keys(time),
+                datasets: [{
+                    label: "Number of Movies",
+                    data: Object.values(time),
+                }],
+            }
+            // options: {
+
+            // }
+        })
+    },
+
+    openNav: function () {
+        $("#navbar").css("width", "400px");
+        $(".nav-content").show();
+        $("#nav-close").css("visibility", "visible");
+        $("#nav-open").css("visibility", "hidden");
+        $(".main").css("margin-left", "425px");
+    },
+
+    closeNav: function () {
+        $("#navbar").css("width", "75px");
+        $(".nav-content").hide();
+        $("#nav-close").css("visibility", "hidden");
+        $("#nav-open").css("visibility", "visible");
+        $(".main").css("margin-left", "80px");
+    },
+};
+
+$(function () {
+    $(window).on('load', function () {
+        App.init();
+    });
+});
