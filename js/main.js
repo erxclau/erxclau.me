4c6b0d01c9414e130e65ee865d253c1ed87b8fb0
ericlau00 <sharkguy418@gmail.com>
2020-06-18 10:50:56 -0400

    Add education section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let links = ['facebook', 'github', 'spotify', 'instagram', 'twitter', 'linkedin'];
 
 for (let i = 0; i < links.length; i++) {
-    let el = `#${links[i]}`
+    let el = document.getElementById(links[i]);
     let color = getComputedStyle(document.documentElement).getPropertyValue(`--${links[i]}-color`);
-    $(el).css("border-bottom-color", color);
-    $(el).mouseover(function () {
-        $(this).css('background-color', color);
+    el.style.borderBottomColor = color;
+    el.addEventListener('mouseover', () => {
+        el.style.backgroundColor = color;
     });
-    $(el).mouseout(function () {
-        $(this).css('background-color', 'transparent');
+    el.addEventListener('mouseout', () => {
+        el.style.backgroundColor = 'transparent';
     });
 }
