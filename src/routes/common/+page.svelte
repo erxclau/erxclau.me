ef7e5734ce1a449edc66520df425dac7ccb7b885
erxclau <ericlau41802@gmail.com>
2026-05-19 20:48:23 -0400

    Update data

--- src/routes/common/+page.svelte
+++ src/routes/common/+page.svelte
@@ -8,6 +8,134 @@
 		<ul>
 			<li>
 				<figure>
+					<blockquote>
+						And I swear, by finding him guilty you will only make it easier for him: you will ease
+						his conscience, he will curse the blood he has shed and will not regret it. At the same
+						time you will destroy in him the possibility of becoming a new man, for he will remain
+						in his wickedness and blindness all his life.
+					</blockquote>
+					<figcaption>
+						Fetyukovich <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+			<li>
+				<figure>
+					<blockquote>
+						I asked just now what does ‘father’ mean, and exclaimed that it was a great word, a
+						precious name. But one must use words honestly, gentlemen, and I venture to call things
+						by their right names: such as father as old Karamazov cannot be called a father and does
+						not deserve to be. Filial love for an unworthy father is an absurdity, an impossibility.
+						Love cannot e created from nothing: only God can create something from nothing.
+					</blockquote>
+					<figcaption>
+						Fetyukovich <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						And if you come to that, does proving there’s a devil prove that there’s a God?
+					</blockquote>
+					<figcaption>
+						The devil <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						Come, you want obedience and mysticism. You must admit that the Christian religion, for
+						instance, has only been of use to the rich and the powerful to keep the lower classes in
+						slavery, that’s so, isn’t it?
+					</blockquote>
+					<figcaption>Kolya Krasotkin <cite>The Brothers Karamazov</cite></figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						Instead of taking possession of men’s freedom, Thou didst increase it, and burdened the
+						spiritual kingdom of mankind with its sufferings for ever. Thou didst desire man’s free
+						love, that he should follow Thee freely, enticed and taken captive by Thee. In place of
+						the rigid ancient law, man must hereafter with free heart decide for himself what is
+						good and what is evil, having only Thy image before him as a guide. But didst Thou not
+						know he would at last reject even Thy image and Thy truth, if he is weighed down with
+						the fearful burden of free choice? They will cry aloud at last that the truth is not in
+						Thee, for they could not have been left in greater confusion and suffering than Thou has
+						caused, laying upon them so many cares and unanswerable problems.
+					</blockquote>
+					<figcaption>
+						The Grand Inquisitor <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						So long as man remains free he strives for nothing so incessantly and so painfully as to
+						find some one to worship. But man seeks to worship what is established beyond dispute,
+						so that all men would agree at once to worship it. For those pitiful creatures are
+						concerned not only to find what one or the other can worship, but to find something that
+						all would believe in and worship; what is essential is that all may be together in it.
+						This craving for community of worship is the chief misery of every man individually and
+						of all humanity from the beginning of time. For the sake of common worship they’ve slain
+						each other with the sword. They have set up gods and challenged one another, ‘Put away
+						your gods and come and worship ours, or we will kill you and your gods!’ And so it will
+						be to the end of the world, even when gods disappear from the earth; they will fall down
+						before idols just the same.
+					</blockquote>
+					<figcaption>The Grand Inquisitor <cite>The Brothers Karamazov</cite></figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						Thou wouldst go into the world, and art going with empty hands, with some promise of
+						freedom which men in their simplicity and their natural unruliness cannot even
+						understand, which they fear and dread—for nothing has ever been more insupportable for a
+						man and a human society than freedom.
+					</blockquote>
+					<figcaption>
+						The Grand Inquisitor <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						Imagine that you are creating a fabric of human destiny with the object of making men
+						happy in the end, giving them peace and rest at last, but that it was essential and
+						inevitable to torture to death only one tiny creature—a baby beating its breast with its
+						fist, for instance—and to found that edifice on its unavenged tears, would you consent
+						to be the architect on those conditions?
+					</blockquote>
+					<figcaption>
+						Ivan Karamazov <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+
+			<li>
+				<figure>
+					<blockquote>
+						Certainly, love [life], regardless of logic as you say, it must be regardless of logic,
+						and it’s only then one will understand the meaning of it.
+					</blockquote>
+					<figcaption>
+						Alyosha Karamazov <cite>The Brothers Karamazov</cite>
+					</figcaption>
+				</figure>
+			</li>
+			<li>
+				<figure>
 					<blockquote>You’re not a visionary, you’re a vendor.</blockquote>
 					<figcaption>
 						Miranda Priestly <cite>The Devil Wears Prada 2</cite>
@@ -37,7 +165,7 @@
 				<figure>
 					<blockquote>
 						People should be able to say how they feel — how they really feel, not, you know, some
-						words that some strangers put in their mouths. Words like “love” that don’t mean
+						words that some strangers put in their mouths. Words like ‘love’ that don’t mean
 						anything. There’s enough bullshit in the world without my help.
 					</blockquote>
 					<figcaption>
