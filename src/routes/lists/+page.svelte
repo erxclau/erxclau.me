e3aab067421238230335878e7e3047f889eef35c
erxclau <ericlau41802@gmail.com>
2026-07-06 21:30:36 -0400

    Update data

--- src/routes/lists/+page.svelte
+++ src/routes/lists/+page.svelte
@@ -549,7 +549,7 @@
 							notes: 'On Disney+',
 							thoughts: `I enjoyed the historical geopolitical aspect of the 
 							movie, and that Magneto is not built as a villain. The accents for
-							apparent Germans continues to feel silly to me.`
+							apparent Germans continue to feel silly to me.`
 						},
 						{
 							name: 'X-Men: The Last Stand',
